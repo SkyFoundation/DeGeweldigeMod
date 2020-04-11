aefcd9b030d68214b0cd539c1caac8ddc242a66f
WisdomJack <Wisdomjack143@outlook.com>
2020-04-11 17:10:44 +0800

    覆盖之前不小心push的操作

--- src/main/java/com/pixelsky/cheese/Reference.java
+++ src/main/java/com/pixelsky/cheese/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MODID = "dgm";
 	public static final String NAME = "6DeGeweldigeMod";
-	public static final String VERSION = "1.11.2.0";
+	public static final String VERSION = "1.11.2.1";
 	public static final String CLIENTPROXY = "com.pixelsky.cheese.proxy.ClientProxy";
 	public static final String COMMONPROXY = "com.pixelsky.cheese.proxy.CommonProxy";
 	
